c1e1227b58a68a44f040ce20e7e4aeae7fe2bcd3
Kwonkipyo <k>
2023-08-29 15:06:32 +0900

    feat : 수정 사항 수정 완료 및 작업 중간 저장(권기표)
    best.css 수정 / common.css 수정 / vision.css 수정 / visual.css 수정 /
    best.js 수정 / visual.js 수정 / index.html 작업 중 / goods.css 작업 중 /

--- js/best.js
+++ js/best.js
@@ -1,10 +1,10 @@
-// 비주얼 슬라이드 기능
-$(document).ready(function () {
-  const swBest = new Swiper(".sw-best", {
-    loop: true,
-    navigation: {
-      prevEl: ".sw-best-prev",
-      nextEl: ".sw-best-next",
-    },
-  });
-});
+// // 비주얼 슬라이드 기능
+// $(document).ready(function () {
+//   const swBest = new Swiper(".sw-best", {
+//     loop: true,
+//     navigation: {
+//       prevEl: ".sw-best-prev",
+//       nextEl: ".sw-best-next",
+//     },
+//   });
+// });
