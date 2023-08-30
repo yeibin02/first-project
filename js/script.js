cc898541894690fbbbe2fc164444666a0eb7d577
miiiiinjeong <mj96.kwon@gmail.com>
2023-08-30 14:56:56 +0900

    feat : (권민정) 전체페이지 스크롤효과적용
    각 페이지의 화면배치 수정이 필요함

--- js/script.js
+++ js/script.js
@@ -13,6 +13,12 @@
 //     };
 // };
 // // dom (html 태그 로딩 완료 후 실행)
-// $(document).ready(function () { });
+$(document).ready(function () {
+  $("#fullpage").fullpage({
+    //options here
+    autoScrolling: true,
+    scrollHorizontally: true,
+  });
+});
 // // 멀티미디어 리소스 로딩 완료 후 실행
 // window.onload = function(){}
