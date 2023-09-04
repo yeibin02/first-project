f5fc73b512f8ed38754e3c8bf024800a313a7b7a
yeibin02 <yeibin02@gmail.com>
2023-09-04 19:35:24 +0900

    feat : event js 수정 (정예빈)

--- js/event.js
+++ js/event.js
@@ -1,38 +1,48 @@
-$(document).ready(function(){
+window.onload = function () {
   function onClick() {
     document.querySelector("#modal .modal-window").style.display = "block";
     document.querySelector(".bg").style.display = "block";
   }
-  
+
   function offClick() {
     document.querySelector("#modal .modal-window").style.display = "none";
     document.querySelector(".bg").style.display = "none";
   }
-  
+
   document.querySelector("#openBtn").addEventListener("click", onClick);
   document.querySelector(".close-area").addEventListener("click", offClick);
-  
+
   const complete = document.getElementById("complete");
   const cancel = document.getElementById("cancel");
-  
+
   complete.addEventListener("click", function () {
-    alert("참여해주셔서 감사합니다!");
-    offClick();
+    if (validateInput()) {
+      alert("참여해주셔서 감사합니다!");
+      offClick();
+    }
   });
-  
+
   cancel.addEventListener("click", function () {
     offClick();
   });
-  
+
   function validateInput() {
-    const idInput = document.getElementById("id").value;
+    const idInput = document.getElementById("user-id").value;
     const emailInput = document.getElementById("email").value;
-  
+
     if (!idInput || !emailInput) {
       alert("아이디와 이메일을 입력해주세요.");
       return false;
     }
-    // 이메일 유효성 검사 로직 추가 (정규식 등)
+
+    // Email validation using a regular expression
+    var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+    if (!emailPattern.test(emailInput)) {
+      alert("올바른 이메일 주소를 입력해주세요.");
+      return false;
+    }
+
     return true;
   }
-});
+};
